21746aa990435029ca1ef07e46ab9ea6383d51b2
ziyuqiu <ziyuqiu@brandeis.edu>
2017-10-13 14:18:33 -0400

    fix leveling directing and modified home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   def get
-  	redirect_to courses_path
+  	redirect_to courses_url
   end
 end
