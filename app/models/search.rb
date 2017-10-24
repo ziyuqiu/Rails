a499341c1f7954922382267375b2cf0626614624
ziyuqiu <ziyuqiu@brandeis.edu>
2017-10-24 05:14:50 -0400

    for push

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,2 @@
 class Search < ApplicationRecord
-	def search_courses
-		# courses = Courses.all
-		# courses = courses.where(["subject_num LIKE ?", Subjects.where(:num => subject_num)]) if subject.present?
-		# return courses
-
 end
