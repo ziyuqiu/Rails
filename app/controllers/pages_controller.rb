9e8605ecd38a4c28a6ce9f1d2d94859c3216c32e
ziyuqiu <ziyuqiu@brandeis.edu>
2017-10-27 15:01:55 -0400

    try fix html

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   def show
     render template: "pages/#{params[:page]}"
-  end
+  #   puts "test1"
+  #   require 'net/http' 
+		# url = URI.parse('http://www.mta.me') 
+		# req = Net::HTTP::Get.new(url.to_s) 
+		# res = Net::HTTP.start(url.host, url.port) {|http| 
+		#    http.request(req)
+		# }
+		# puts res.code
+		# @code = res.body 
+		# puts @code
+  	end
+
 end
